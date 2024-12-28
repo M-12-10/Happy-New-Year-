b744027ef5b23f9eac857df11cde609465b22194
M-12-10 <buttwahaj110@gmail.com>
2024-12-28 06:57:57 +0000

    2

--- src/components/recap/PhotoModal.tsx
+++ src/components/recap/PhotoModal.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { X, ChevronLeft, ChevronRight, Download, Share2 } from "lucide-react";
+import { ChevronLeft, ChevronRight, Download, Share2 } from "lucide-react";
 
 interface PhotoModalProps {
   isOpen?: boolean;
@@ -20,30 +15,17 @@ interface PhotoModalProps {
 const PhotoModal = ({
   isOpen = true,
   onClose = () => {},
+  photoUrl,
   onPrevious = () => {},
   onNext = () => {},
 }: PhotoModalProps) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="max-w-[90vw] max-h-[90vh] p-0 bg-pink-50">
-        <DialogHeader className="p-4 absolute top-0 left-0 right-0 z-10 bg-gradient-to-b from-black/50 to-transparent">
-          <DialogTitle className="text-white flex justify-between items-center">
-            <span>{photoTitle}</span>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="text-white hover:bg-white/20"
-              onClick={onClose}
-            >
-              <X className="h-6 w-6" />
-            </Button>
-          </DialogTitle>
-        </DialogHeader>
-
         <div className="relative w-full h-[80vh] bg-pink-100">
           <img
             src={photoUrl}
-            alt={photoTitle}
+            alt="Photo"
             className="w-full h-full object-contain"
           />
 
@@ -86,15 +68,6 @@ const PhotoModal = ({
             >
               <Share2 className="h-4 w-4" />
             </Button>
-          </div>
-
-          {/* Hello Kitty decorative element */}
-          <div className="absolute top-4 right-4">
-            <img
-              src="https://api.dicebear.com/7.x/avataaars/svg?seed=kitty"
-              alt="Hello Kitty decoration"
-              className="w-12 h-12 opacity-70"
-            />
           </div>
         </div>
       </DialogContent>
